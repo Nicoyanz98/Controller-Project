63bfa39276d81fe6091b397fc9f387319db74fa0
Nicoyanz <nicyanz98@gmail.com>
2025-12-21 20:17:25 -0300

    Drawing hand keypoints

--- hand_viz/visualization.py
+++ hand_viz/visualization.py
@@ -50,9 +50,7 @@ class JoystickDetector:
         self.start_thread("controller")
         self.start_thread("hands")
 
-    def display_boxes(self, display_frame, detection_name):
-        current_results = self.mutex[detection_name].get()
-        
+    def display_boxes(self, display_frame, current_results, class_names):
         if (current_results is not None and current_results.boxes is not None and len(current_results.boxes) > 0):
             boxes = current_results.boxes.xyxy.cpu().numpy()
             classes = current_results.boxes.cls.cpu().numpy()
@@ -60,14 +58,34 @@ class JoystickDetector:
             
             for _, (box, class_id, confidence) in enumerate(zip(boxes, classes, confidences)):
                 x1, y1, x2, y2 = map(int, box)
-                class_id = int(class_id)
-                class_name = self.threads[detection_name].model.names[class_id]
+                class_name = class_names[int(class_id)]
                 confidence = float(confidence)
                 
                 cv2.rectangle(display_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 label = f"{class_name}: {confidence:.2f}"
                 cv2.putText(display_frame, label, (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+        
+    def display_controller(self, display_frame):
+        results = self.mutex["controller"].get()
+        self.display_boxes(display_frame, results, self.threads["controller"].model.names)
+    
+    def display_hands(self, display_frame):
+        results = self.mutex["hands"].get()
+        
+        self.display_boxes(display_frame, results, self.threads["hands"].model.names)
+        if results is not None and results.keypoints is not None:
+            h, w = results.keypoints.orig_shape
 
+            kpts_xyn = results.keypoints.xyn.cpu().numpy()
+            kpts_conf = results.keypoints.conf.cpu().numpy()
+
+            for hand_id in range(kpts_xyn.shape[0]):
+                for (xn, yn), conf in zip(kpts_xyn[hand_id], kpts_conf[hand_id]):
+                    if conf > 0.5:
+                        x = int(xn * w)
+                        y = int(yn * h)
+                        cv2.circle(display_frame, (x, y), 4, (0, 0, 255), -1)
+        
     def display_fps(self, display_frame, current_time):
         self.fps_count += 1 #Contamos los frames
         if current_time - self.fps_time >= 1.0: #Si paso mas de un segundo, actuazamos el contador
@@ -97,8 +115,8 @@ class JoystickDetector:
                 if current_frame is not None:
                     display_frame = current_frame.copy()
                     # Detection Boxes
-                    self.display_boxes(display_frame, "controller")
-                    self.display_boxes(display_frame, "hands")
+                    self.display_controller(display_frame)
+                    self.display_hands(display_frame)
 
                     # FPS
                     self.display_fps(display_frame, current_time)
